f2805a8f3498a283d9435cac3aaf88d465e86103
Vinnie Leung <vleung20@gmail.com>
2024-05-06 15:29:36 +0100

    Add project files.

---
+++ HPFizzBuzzer/FizzBuzzer.cs
@@ -0,0 +1,22 @@
+﻿namespace HPFizzBuzzer
+{
+    internal class FizzBuzzer
+    {
+        public string DetermineFizzBuzz(int number)
+        {
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else return $"{number}";
+        }
+    }
+}
---
+++ HPFizzBuzzer/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HPFizzBuzzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzer fb = new FizzBuzzer();
+            Enumerable.Range(1, 100).ToList().ForEach(c => Console.WriteLine(fb.DetermineFizzBuzz(c)));
+
+        }
+    }
+}
