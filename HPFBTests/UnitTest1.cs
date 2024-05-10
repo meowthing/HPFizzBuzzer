47dc0b2a2642c1c476aebe56ff8ad64376177990
Vinnie Leung <vleung20@gmail.com>
2024-05-10 16:43:25 +0100

    Added more tests and alternative test structure styles

--- HPFBTests/UnitTest1.cs
+++ HPFBTests/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace HPFBTests
         {
             List<IFizzBuzzer> fizzBuzzers = new List<IFizzBuzzer>
             {
-                new Fizz(), new Buzz()
+                new Fizz(), new Buzz(), new Fazz()
             };
             fb = new FizzBuzzer(fizzBuzzers);
         }
@@ -19,19 +19,37 @@ namespace HPFBTests
         [Test]
         public void TestFizz()
         {
-            Assert.That(fb.DetermineFizzBuzz(3), Is.EqualTo("Fizz"));
+            Assert.That(fb.DetermineFizzBuzz(3), Does.Contain("Fizz"));
         }
 
         [Test]
         public void TestBuzz()
         {
-            Assert.That(fb.DetermineFizzBuzz(5), Is.EqualTo("Buzz"));
+            Assert.That(fb.DetermineFizzBuzz(5), Does.Contain("Buzz"));
         }
 
         [Test]
         public void TestFizzBuzz()
         {
-            Assert.That(fb.DetermineFizzBuzz(15), Is.EqualTo("FizzBuzz"));
+            Assert.That(fb.DetermineFizzBuzz(15), Does.Contain("FizzBuzz"));
+        }
+
+        [Test]
+        public void TestFazz()
+        {
+            Assert.That(fb.DetermineFizzBuzz(7), Does.Contain("Fazz"));
+        }
+
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(3, "Fizz")]
+        [TestCase(7, "Fazz")]
+        [TestCase(70, "BuzzFazz")]
+        [TestCase(84, "FizzFazz")]
+        public void TestCases(int number, string word)
+        {
+            Assert.That(fb.DetermineFizzBuzz(number), Does.Contain(word));
         }
 
         [Test]
